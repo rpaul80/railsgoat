b269a048f2228443879a9f930dcf98aff550dcff
Raj Paul <raj@boostsecurity.io>
2021-02-12 11:06:25 -0500

    removing some

--- bad.rb
+++ bad.rb
@@ -9,9 +9,5 @@ hash = Digest::MD5.hexdigest(five) # noboost
 hash = Digest::MD5.hexdigest(six)
 hash = Digest::MD5.hexdigest(seven)
 hash = Digest::MD5.hexdigest(8t)
-hash = Digest::MD5.hexdigest("foooo")
-hash = Digest::MD5.hexdigest("roooo")
-hash = Digest::MD5.hexdigest("sroooo")
-
 
 
